92b741446583fd840ba4c4b6bca48b705e2829b3
Vinicius Lenartevitz <111529145+VLenartevitz@users.noreply.github.com>
2024-03-14 21:45:45 -0300

    Add files via upload

---
+++ trab1ldw/trab1ldw/controllers/routes.py
@@ -0,0 +1,33 @@
+from flask import render_template, request
+
+jogadores = []
+jogos = []
+cursosList = [{'nome' : 'google cloud', 'horas' : 20, 'professor' : 'Deus'}]
+livroslist = [{'Livros' : 'Harry'}]
+
+
+def init_app(app):
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    @app.route('/perfil', methods=['GET', 'POST'])
+    def perfil():
+        if request.method == 'POST':
+            if request.form.get('nome') and request.form.get('horas') and request.form.get('professor'):
+                cursosList.append({'nome' : request.form.get('nome'), 'horas' : request.form.get('horas'), 'professor' : request.form.get('professor')})
+        return render_template('perfil.html',cursosList=cursosList)
+    
+
+    @app.route('/livros', methods=['GET', 'POST'])
+    def livros():
+        livro = livroslist[0]
+      
+        if request.form.get('jogo'):
+            jogos.append(request.form.get('jogo'))
+                
+        return render_template('livros.html',livro=livro, jogadores=jogadores, jogos=jogos
+)
+    
+
+  
